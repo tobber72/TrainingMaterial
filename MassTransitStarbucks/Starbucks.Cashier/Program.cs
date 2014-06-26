9eea86cab71372ef63b0101edaa2d2f01da1d2d8
Jason Weibel <Jason.a.weibel@gmail.com>
2014-06-26 15:59:54 -0500

    continue moving to unity ioc

--- MassTransitStarbucks/Starbucks.Cashier/Program.cs
+++ MassTransitStarbucks/Starbucks.Cashier/Program.cs
@@ -18,8 +18,11 @@ namespace Starbucks.Cashier
     using Magnum;
     using Magnum.StateMachine;
     using MassTransit.Log4NetIntegration.Logging;
-    using Ninject;
+
     using Topshelf;
+    using Microsoft.Practices.Unity;
+    using MassTransit.Saga;
+    using MassTransit;
 
     internal static class Program
     {
@@ -31,28 +34,50 @@ namespace Starbucks.Cashier
         {
             Log4NetLogger.Use("cashier.log4net.xml");
 
+            var container = new UnityContainer();
+
+            // register types directly
+            container.RegisterType<CashierService>(new ContainerControlledLifetimeManager());
+            container.RegisterType(typeof(ISagaRepository<>), typeof(InMemorySagaRepository<>), new ContainerControlledLifetimeManager());
+
+            // Register the ServiceBus.
+            container.RegisterInstance<IServiceBus>(ServiceBusFactory.New(sbc =>
+            {
+                // Configure exchanges.
+                sbc.ReceiveFrom("msmq://localhost/starbucks_cashier");
+                sbc.UseMsmq();
+                sbc.UseMulticastSubscriptionClient();
+
+                sbc.SetConcurrentConsumerLimit(1); //a cashier cannot multi-task
+                sbc.UseControlBus();
+                sbc.EnableRemoteIntrospection();
+
+                sbc.Subscribe(subs => { subs.LoadFrom(container); });
+            }));
+
+
             HostFactory.Run(c =>
+            {
+                c.SetServiceName("StarbucksCashier");
+                c.SetDisplayName("Starbucks Cashier");
+                c.SetDescription("a Mass Transit sample service for handling orders of coffee..");
+
+                c.DependsOnMsmq();
+                c.RunAsLocalService();
+
+                DisplayStateMachine();
+
+                c.Service<CashierService>(s =>
                 {
-                    c.SetServiceName("StarbucksCashier");
-                    c.SetDisplayName("Starbucks Cashier");
-                    c.SetDescription("a Mass Transit sample service for handling orders of coffee.");
-
-                    c.RunAsLocalSystem();
-                    c.DependsOnMsmq();
-
-                    var kernel = new StandardKernel();
-                    var module = new CashierRegistry();
-                    kernel.Load(module);
-
-                    DisplayStateMachine();
-
-                    c.Service<CashierService>(s =>
-                        {
-                            s.ConstructUsing(builder => kernel.Get<CashierService>());
-                            s.WhenStarted(o => o.Start());
-                            s.WhenStopped(o => o.Stop());
-                        });
+                    s.ConstructUsing(builder => container.Resolve<CashierService>());
+                    s.WhenStarted(o => o.Start());
+                    s.WhenStopped(o =>
+                    {
+                        o.Stop();
+                        container.Dispose();
+                    });
                 });
+            });
         }
 
         static void DisplayStateMachine()
